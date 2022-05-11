f95f2578abc1db1f273e8b8967b17969e4a961ec
Niloy Mahmud <mahmudniloy78@gmail.com>
2022-05-11 14:13:27 +0600

    latest version

--- js/script.js
+++ js/script.js
@@ -1,26 +1,10 @@
 new Vue({
     el: '#app',
     data: {
-        mainTitle: "",
-        mainSlogan: "",
         poetryName: "",
         poetName: "",
         date: "",
-        adjust: "",
     },
-});
-
-// Uploading LogoImages
-const imageInput = document.querySelector("#imageInput");
-var uploadedImage = "";
-
-imageInput.addEventListener("change", function () {
-    const reader = new FileReader();
-    reader.addEventListener("load", () => {
-        uploadedImage = reader.result
-        document.querySelector("#displayImage").style.backgroundImage = `url(${uploadedImage})`;
-    });
-    reader.readAsDataURL(this.files[0]);
 });
 
 // Uploading ProfileImages
